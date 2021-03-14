3dcc671e1542bb5fb2cbb7ae87838045d636acb8
DarkKeksik <frilans.er@mail.ru>
2021-03-15 00:00:51 +0300

    Added preloader for page post

--- pages/post/[id].js
+++ pages/post/[id].js
@@ -1,19 +1,47 @@
+import { useState, useEffect } from "react";
 import {MainLayout} from "../../Components/layouts";
+import Link from "next/link";
+import {useRouter} from "next/router";
 
-export default function Post({ post }) {
+export default function Post({ post: serverPost }) {
+    const [ post, setPost ] = useState( serverPost );
+    const router = useRouter();
+
+    useEffect(()=>{
+        async function load() {
+            const response = await fetch(`http://localhost:4200/posts/${ router.query.id }`);
+            const data = await response.json();
+            setPost(data);
+        }
+
+        !serverPost && load();
+    }, []);
+
+    if ( !post ) {
+        return (
+            <MainLayout>
+                <p>Loading...</p>
+            </MainLayout>
+        )
+    }
+
     return (
         <MainLayout title={ post.title }>
             <div>
                 <h1>{ post.title }</h1>
                 <div>{ post.description }</div>
+                <Link href={"/posts"}>
+                    <a>Back to all posts</a>
+                </Link>
             </div>
         </MainLayout>
     )
 }
 
-Post.getInitialProps = async ctx => {
-    const postId = ctx.query.id;
-    const response = await fetch(`http://localhost:4200/posts/${ postId }`);
+Post.getInitialProps = async ({ query, req }) => {
+    if( !req ) return { post: null }
+
+    const response = await fetch(`http://localhost:4200/posts/${ query.id }`);
     const post = await response.json();
 
     return { post }
